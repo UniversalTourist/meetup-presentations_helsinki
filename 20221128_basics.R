85d1cce99e881ed78b3e1caf9b24c19b8d24f82e
Hazel Kavılı <hazelkavili@icloud.com>
2022-12-04 11:39:32 +0200

    basics examples added

---
+++ 20221128_basics.R
@@ -0,0 +1,24 @@
+#this is R-Ladies Helsinki
+A <- 10
+a <- 3
+A
+a
+
+
+myNumbers <- c(1:10)
+rep(myNumbers, times = 3)
+twice <- rep(myNumbers, each = 2)
+
+
+unique(game_data$game_name)
+
+game_data %>% 
+  count(game_name)
+
+
+
+
+
+
+
+
